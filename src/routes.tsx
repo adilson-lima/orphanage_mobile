4249b556d0725fecfe4848a765de204f4dd18b95
Adilson Pereira Lima <adilson.pereira.lima@gmail.com>
2021-02-25 16:39:46 -0300

    Project orphanage development in the NLW - Next Level Week.

---
+++ src/routes.tsx
@@ -0,0 +1,55 @@
+import React from 'react'
+
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator} from '@react-navigation/stack'
+
+import OrphanagesMap from './pages/OrphanagesMap'
+import OrphanagesDetails from './pages/OrphanagesDetails'
+import OrphanageData from './pages/createOrphanage/OrphanageData'
+import SelectMapPosition from './pages/createOrphanage/SelectMapPosition'
+import Header from './componentes/Header'
+
+
+const { Navigator, Screen } = createStackNavigator()
+
+
+
+export default function Routes() {
+    return(
+        <NavigationContainer>
+            <Navigator screenOptions={{ headerShown: false, cardStyle: {backgroundColor: '#f2f3f5'}}}>
+                
+                <Screen name="OrphanagesMap" component={OrphanagesMap}/>
+                
+                <Screen 
+                    name="OrphanagesDetails" 
+                    component={OrphanagesDetails}
+                    options = {{
+                        headerShown: true,
+                        header: ()=> <Header title="Orfanato" showCancel={false}/>
+                    }}
+                />
+
+                <Screen 
+                    name="SelectMapPosition" 
+                    component={SelectMapPosition}
+                    options = {{
+                        headerShown: true,
+                        header: ()=> <Header title="Selecione o mapa"/>
+                    }}
+                />
+                
+                <Screen 
+                    name="OrphanageData" 
+                    component={OrphanageData}
+                    options = {{
+                        headerShown: true,
+                        header: ()=> <Header title="Informe os dados"/>
+                    }}
+                />
+                
+
+            </Navigator>
+        </NavigationContainer>
+    )
+}
